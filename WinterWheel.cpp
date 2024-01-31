d4a187550c2b6e19c7ad43df256b0b4728c1c5fe
Kamel R <kamel.r@wayne.edu>
2024-01-31 18:41:10 -0500

    added the code for the WinterWheel class

---
+++ WinterWheel.cpp
@@ -0,0 +1,21 @@
+#include "WinterWheel.h"
+
+void WinterWheel::setThreadSize(float t)
+{
+    threadSize = t;
+}
+
+float WinterWheel::getThreadSize()
+{
+    return threadSize;
+}
+
+WinterWheel::WinterWheel():Wheel()
+{
+    threadSize = 1;
+}
+
+WinterWheel::WinterWheel(string make, float diameter, float threadSize):Wheel(make,diameter)
+{
+    this->threadSize = threadSize;
+}
---
+++ WinterWheel.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Wheel.h"
+class WinterWheel : public Wheel
+{
+private:
+	float threadSize;
+
+public:
+
+	void setThreadSize(float t);
+	float getThreadSize();
+
+	WinterWheel();
+	WinterWheel(string make, float diameter, float threadSize);
+};
+
